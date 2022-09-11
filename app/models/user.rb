3b813255befbffe753c59b1e76622311a134c00b
Alexey Guriev <guriev06@gmail.com>
2022-09-11 22:55:22 +0300

    Guests' subs update when registered

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   before_validation :set_name, on: :create
+  after_commit :link_subscriptions, on: :create
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -14,6 +16,11 @@ class User < ApplicationRecord
   validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 
   private
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+                .update_all(user_id: self.id)
+  end
 
   def set_name
     self.name = "Господин(жа) №#{rand(999)}" if self.name.nil?
