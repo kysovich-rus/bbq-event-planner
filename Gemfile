4744e3f64aa7f1834dc8ca29e5641f0338930841
Alexey Guriev <guriev06@gmail.com>
2022-09-18 16:11:03 +0300

    Yandex Maps script now loads directly at view

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem "turbo-rails"
 
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
+gem "hotwire-rails"
 
 # Bundle and process CSS [https://github.com/rails/cssbundling-rails]
 gem "cssbundling-rails"
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 import "./controllers"
-import * as bootstrap from "bootstrap"
+import "@hotwired/turbo-rails"
