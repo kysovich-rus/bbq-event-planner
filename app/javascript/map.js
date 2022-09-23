87ffbfe6ebf7cbd865a5cab2f090eda32ee7ae2c
Alexey Guriev <guriev06@gmail.com>
2022-09-24 01:16:31 +0300

    map.js guard clause

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -2,35 +2,34 @@ document.addEventListener('turbo:load', () => {
   ymaps.ready(init);
 
   function init() {
-    if (document.getElementById('map')) {
-      const address = document.getElementById('map').getAttribute('data-address');
+    mapElement = document.getElementById('map');
+    if (!mapElement) return false;
+    const address = mapElement.dataset.address;
+    const myMap = new ymaps.Map("map", {
+      center: [55.76, 37.64],
+      zoom: 10
+    });
 
-      const myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 10
-      });
+    myGeocoder = ymaps.geocode(address);
 
-      myGeocoder = ymaps.geocode(address);
+    myGeocoder.then(
+      function (res) {
+        const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-      myGeocoder.then(
-        function (res) {
-          const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+        myMap.geoObjects.add(
+          new ymaps.Placemark(
+            coordinates,
+            {iconContent: address},
+            {preset: 'islands#blueStretchyIcon'}
+          )
+        );
 
-          myMap.geoObjects.add(
-            new ymaps.Placemark(
-              coordinates,
-              {iconContent: address},
-              {preset: 'islands#blueStretchyIcon'}
-            )
-          );
-
-          myMap.setCenter(coordinates);
-          myMap.setZoom(15);
-        }, function (err) {
-          alert('Ошибка при определении местоположения');
-        }
-      );
-    }
+        myMap.setCenter(coordinates);
+        myMap.setZoom(15);
+      }, function (err) {
+        alert('Ошибка при определении местоположения');
+      }
+    );
   }
 })
 
